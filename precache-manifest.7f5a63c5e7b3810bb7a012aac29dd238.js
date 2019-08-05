ffeda5a3809f0a098da2db171a7eea0a9a00723e
Michael Sung (宋昶松) <michaelsung@cybersoft4u.com.tw>
2019-08-05 12:19:47 +0800

    Updates

---
+++ precache-manifest.7f5a63c5e7b3810bb7a012aac29dd238.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9714e89c11db79e2192130f746d68ae9",
+    "url": "/React_practice/index.html"
+  },
+  {
+    "revision": "2c9d238432917b41f9ef",
+    "url": "/React_practice/static/js/2.634742f5.chunk.js"
+  },
+  {
+    "revision": "b6c09b24cf8d06ea4501",
+    "url": "/React_practice/static/js/main.a3882e10.chunk.js"
+  },
+  {
+    "revision": "283161e0992e8796afc2",
+    "url": "/React_practice/static/js/runtime~main.b745a6b8.js"
+  },
+  {
+    "revision": "e249914e9d3c8c92e0059c063c950353",
+    "url": "/React_practice/static/media/baseline-done-24px.e249914e.svg"
+  },
+  {
+    "revision": "14b207a7cff835230b2e5899a1ec8e31",
+    "url": "/React_practice/static/media/dog.14b207a7.jpg"
+  },
+  {
+    "revision": "cebb246c7af454eb839f7a80532f2918",
+    "url": "/React_practice/static/media/door1_f.cebb246c.jpg"
+  },
+  {
+    "revision": "095ef2347a1a674fecd5e391c9e13233",
+    "url": "/React_practice/static/media/door3_f.095ef234.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React_practice/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "76dcedd9d0e23af86c5f6522a4a34e4f",
+    "url": "/React_practice/static/media/win.76dcedd9.jpg"
+  }
+]);
